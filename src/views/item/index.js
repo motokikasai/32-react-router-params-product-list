3d9ae80492b5caf7c50469ac3a3b73d36d1ef380
motokikasai <kmomoky@hotmail.com>
2020-05-15 22:36:00 +0200

    Handle cases of non-existing q param inputs with Route leaving to error msg

--- src/views/item/index.js
+++ src/views/item/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import backHomeIcon from "../../pointing-left.png";
 import data from "../../data/products.json";
 
@@ -16,38 +17,50 @@ export default class Item extends Component {
       return item.id === this.props.match.params.id;
     });
 
-    this.setState({
-      product: objData,
-    });
+    if (objData) {
+      this.setState({
+        product: objData,
+      });
+    } else {
+      this.setState({
+        product: null,
+      });
+    }
   }
 
   render() {
     return (
-      <div className="item-detail">
-        <div className="list-header">
-          <button onClick={this.toPrev} className="btn-back">
-            <img
-              src={backHomeIcon}
-              alt="back-to-home-button"
-              className="left-arrow-back"
-            />
-          </button>
-          <h1 className="list-title">{this.state.product.name}</h1>
-        </div>
+      <BrowserRouter>
+        {this.state.product ? (
+          <div className="item-detail">
+            <div className="list-header">
+              <button onClick={this.toPrev} className="btn-back">
+                <img
+                  src={backHomeIcon}
+                  alt="back-to-home-button"
+                  className="left-arrow-back"
+                />
+              </button>
+              <h1 className="list-title">{this.state.product.name}</h1>
+            </div>
 
-        <img
-          src={this.state.product.image}
-          alt="product"
-          className="product-image"
-        />
-        <div className="text-wrapper">{this.state.product.description}</div>
-        <p className="price">
-          {`${new Intl.NumberFormat("de-DE", {
-            style: "currency",
-            currency: "EUR",
-          }).format(this.state.product.price)}`}
-        </p>
-      </div>
+            <img
+              src={this.state.product.image}
+              alt="product"
+              className="product-image"
+            />
+            <div className="text-wrapper">{this.state.product.description}</div>
+            <p className="price">
+              {`${new Intl.NumberFormat("de-DE", {
+                style: "currency",
+                currency: "EUR",
+              }).format(this.state.product.price)}`}
+            </p>
+          </div>
+        ) : (
+          <Route>Error: 404 Not Found</Route>
+        )}
+      </BrowserRouter>
     );
   }
 }
